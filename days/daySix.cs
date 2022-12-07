d514ef8d13ad227624d87274adbb3a11efc8d64f
Brian Toye <btoye.dev@outlook.com>
2022-12-07 21:32:28 +0000

    Cleaner day 6

--- days/daySix.cs
+++ days/daySix.cs
@@ -1,52 +1,42 @@
 public class daySix
 {
     private string[] inputFile;
+    private string packet;
     public daySix()
     {
         inputFile = File.ReadAllLines("./days/daySixInputFile.txt");
+        packet = inputFile[0];
+    }
+
+    private void CharsToParseForUniqueString(int numberOfUniqueChars)
+    {
+        var pointer = numberOfUniqueChars;
+
+        while ((pointer-numberOfUniqueChars) <= packet.Length)
+        {
+            var range = new Range((pointer-numberOfUniqueChars), pointer);
+            var chars = packet.Take(range);
+
+            if (chars.Distinct().Count() != numberOfUniqueChars)
+            {
+                pointer += 1;
+                continue;
+            }
+            else
+            {
+                Console.WriteLine(pointer.ToString());
+                break;
+            }
+        }
     }
 
     public void PartOne()
     {
-        var line = inputFile[0];
-        var pointer = 4;
-        while ((pointer-4) <= line.Length)
-        {
-            var range = new Range((pointer-4), pointer);
-            var chars = line.Take(range);
-
-            if (chars.Distinct().Count() != 4)
-            {
-                pointer += 1;
-                continue;
-            }
-            else
-            {
-                Console.WriteLine(pointer.ToString());
-                break;
-            }
-        }
+        CharsToParseForUniqueString(4);
     }
 
     public void PartTwo()
     {
-        var line = inputFile[0];
-        var pointer = 14;
-        while ((pointer-14) <= line.Length)
-        {
-            var range = new Range((pointer-14), pointer);
-            var chars = line.Take(range);
-
-            if (chars.Distinct().Count() != 14)
-            {
-                pointer += 1;
-                continue;
-            }
-            else
-            {
-                Console.WriteLine(pointer.ToString());
-                break;
-            }
-        }
+        CharsToParseForUniqueString(14);
     }
 }
